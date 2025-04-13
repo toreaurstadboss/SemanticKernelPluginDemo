9da3521a6ee6fbffc4e2e95d9855e4643be15f2c
Tore Aurstad <tore.aurstad@gmail.com>
2025-04-14 01:13:16 +0200

    init commit

--- SemanticKernelPluginDemo/Program.cs
+++ SemanticKernelPluginDemo/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
 using SemanticKernelPluginDemo;
+using SemanticKernelPluginDemo.Data.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+
+// Add DbContext
+builder.Services.AddDbContext<NorthwindContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 await builder.Build().RunAsync();
